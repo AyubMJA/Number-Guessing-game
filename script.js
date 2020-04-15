abaee82683a81876088b3eda05bac4ac9ef41535
Ayub <ayubali8448@icloud.com>
2020-04-15 14:12:51 -0400

    Added compare guesses and update score functions

--- script.js
+++ script.js
@@ -4,4 +4,21 @@ let currentRoundNumber = 1;
 
 // Write your code below:
 
+let generateTarget = () =>{
+    return Math.floor(Math.random() * 9);
+};
+let compareGuesses = (humanGuess,computerGuess,secretTargetNumber) =>{
+    let human = Math.abs(secretTargetNumber - humanGuess);
+    let computer = Math.abs(secretTargetNumber - computerGuess)
+
+    return human <= computer;
+};
+
+let updateScore = (winner) =>{
+    if(winner === 'human'){
+        humanScore++
+    }else if(winner === 'computer'){
+        compareGuesses++
+    }
+}
 
